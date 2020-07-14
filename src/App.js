d34191476f5a62a8e0a0d7d873a87c371dee2445
iLoveCoding <865713897@qq.com>
2020-07-14 15:04:47 +0800

    事件处理

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from 'react';
 
 class App extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {isLoggle: true};
+    this.handleClick = this.handleClick.bind(this);
+  }
 
+  handleClick() {
+    this.setState( state => ({
+      isLoggle: !state.isLoggle
+    }))
+  }
+
+  render() {
+    return(
+      <button onClick={this.handleClick}>
+        {this.state.isLoggle? "ON" : "OFF"}
+      </button>
+    )
+  }
 }
 
 export default App;
